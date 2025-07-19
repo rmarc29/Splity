202292b75247c636cd788f8864980c6fdf18b365
rmarc29 <romain.marc29@gmail.com>
2025-07-19 14:18:41 +0200

    fix

--- Splity.lua
+++ Splity.lua
@@ -113,7 +113,7 @@ end)
 
 
 -- Slash command to show/hide the frame
-SLASH_SPLITY1 = "/splity"
+SLASH_SPLITY = "/splity"
 SlashCmdList["SPLITY"] = function()
     if SplityFrame:IsShown() then
         SplityFrame:Hide()
